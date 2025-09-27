f9b11d6a56fcbf893be910e525dcc93056c88379
Saneha62 <saneharana23bai70062@gmail.com>
2025-09-27 14:59:09 +0530

    commit

---
+++ reactapp/experiment-7/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer({ copyrightYear }) {
+ return (
+ <footer>
+ <p>&copy; {copyrightYear} My React App. All rights reserved.</p>
+ </footer>
+ );
+}
+
+export default Footer;
---
+++ reactapp/experiment-7/src/components/Header.jsx
@@ -0,0 +1,12 @@
+function Header({ title }) {
+ return (
+<div>
+ <Header />
+ <h1>Hello World</h1>
+ <p>Let us learn ReactJS</p>
+ <Footer />
+ </div>
+ );
+}
+
+export default Header;
